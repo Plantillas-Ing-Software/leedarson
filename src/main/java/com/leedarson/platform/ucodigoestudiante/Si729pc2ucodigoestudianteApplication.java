b6c5530cbdc8283c89a666869dff73c760f183af
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-06 00:07:39 -0500

    feat(shared): add infrastructure layer for shared bounded context.

--- src/main/java/com/leedarson/platform/ucodigoestudiante/Si729pc2ucodigoestudianteApplication.java
+++ src/main/java/com/leedarson/platform/ucodigoestudiante/Si729pc2ucodigoestudianteApplication.java
@@ -2,7 +2,19 @@ package com.leedarson.platform.ucodigoestudiante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Si729pc2ucodigoestudianteApplication
+ *
+ * @summary
+ * The main class of the Leedarson Platform application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class Si729pc2ucodigoestudianteApplication {
 
