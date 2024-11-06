ad3a49a8ed1b57c755f3d5389bd03383f81826a2
XaerickS <u202213358@upc.edu.pe>
2024-11-05 23:55:17 -0500

    chore: initial commit

---
+++ src/main/java/com/leedarson/platform/ucodigoestudiante/Si729pc2ucodigoestudianteApplication.java
@@ -0,0 +1,13 @@
+package com.leedarson.platform.ucodigoestudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729pc2ucodigoestudianteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729pc2ucodigoestudianteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leedarson/platform/ucodigoestudiante/Si729pc2ucodigoestudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leedarson.platform.ucodigoestudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729pc2ucodigoestudianteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
